205ab492c1865b181fdd3bf2870c486062aa450a
NileshBarandwal <nbarandwal@gmail.com>
2021-08-21 13:18:57 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Gallery from './Components/gallery'
+import Index from './Components/index'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <Gallery /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
